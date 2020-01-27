319aaaf591d00cf6e8540414bfacb38e1fceb5a5
Charles Estabrooks <charles@estabrooks.org>
2020-01-27 17:09:26 -0500

    Shooter CAD IDs, ControlPanel TalonSRX, CAD ID

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -26,7 +26,7 @@ namespace ConAuto {
 
 namespace ConClimber {
     //Motors
-    constexpr int CLIMB_MOTOR = 6;
+    constexpr int CLIMB_MOTOR = 9;
 }
 
 namespace ConControlPanel {
@@ -55,7 +55,7 @@ namespace ConNEO {
 namespace ConShooter {
     constexpr int DEAULT_SPEED = 4000; //RPM
     namespace Top {
-        constexpr int MOTOR = 0;
+        constexpr int MOTOR = 6;
         constexpr int WHEEL_SIZE = 4; //in inches
         constexpr double VELOCITY_FACTOR = 1; //(ConMath::PI*WHEEL_SIZE) * ConMath::METERS_2_INCH * ConMath::MINUTES_2_SECONDS; //(velocity) y [m/s] = PI*WHEEL_SIZE * m/in * 1/60 * x [RPM]
         //PID gains
@@ -65,7 +65,7 @@ namespace ConShooter {
         constexpr double FF = 1.7e-4;
     }
     namespace Bottom {
-        constexpr int MOTOR = 1;
+        constexpr int MOTOR = 2;
         constexpr int WHEEL_SIZE = 6; //in inches
         constexpr double VELOCITY_FACTOR = 1; //(ConMath::PI*WHEEL_SIZE) * ConMath::METERS_2_INCH * ConMath::MINUTES_2_SECONDS; //(velocity) y [m/s] = PI*WHEEL_SIZE * m/in * 1/60 * x [RPM]
         //PID gains
